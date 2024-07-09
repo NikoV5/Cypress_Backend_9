4e720d2c68ca5fab0323bbc36e59d94160a42f1a
NikoV5 <nikovourtsis7@gmail.com>
2024-07-08 23:41:01 -0500

    Updated files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,14 +12,23 @@
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('validateResponse', (responseData, expectedData) => {
+    Object.entries(expectedData).forEach(([key, value]) => {
+      cy.log(key + ' ' + value + ' This is the key value pairs of Request body')
+      cy.log(responseData.body[key])
+  
+      expect(responseData.body[key]).to.equal(value)
+    })
+  })
+  //
+  // -- This is a child command --
+  // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+  //
+  //
+  // -- This is a dual command --
+  // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+  //
+  //
+  // -- This will overwrite an existing command --
+  // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
